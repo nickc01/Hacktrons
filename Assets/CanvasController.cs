cd41c3e0ee1f44cdb209da7bced45cdff5cb0136
nrcullen98 <nickc01@outlook.com>
2019-01-11 23:53:57 -0600

    Initial Setup

---
+++ Assets/CanvasController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class CanvasController : MonoBehaviour {
+
+    // Use this for initialization
+    private static CanvasGroup groupController;
+	void Start ()
+    {
+        groupController = GetComponent<CanvasGroup>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+	}
+
+    public static async Task Move(GameObject AwayFrom,GameObject Towards)
+    {
+        await Task.Run(() => {
+            CanvasGroup awayGroup = AwayFrom.GetComponent<CanvasGroup>();
+            CanvasGroup towardsGroup = Towards.GetComponent<CanvasGroup>();
+            towardsGroup.alpha = 0;
+            Towards.SetActive(true);
+            float TowardsZ = 300;
+            float AwayZ = 0;
+            Towards.transform.localPosition = new Vector3(0, 0, 300);
+        });
+    }
+}
